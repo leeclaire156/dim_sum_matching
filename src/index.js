462ba01ef5bb686dc8d4c0ebf342dc4f105acf13
Claire <lee.claire156@gmail.com>
2025-09-15 09:16:12 -0400

    Fixed class from 'flip' to 'flipped' that is added to make the cards flip

--- src/index.js
+++ src/index.js
@@ -46,7 +46,7 @@ function flipCard() {
     if (lockBoard) return;
     if (this === firstCard) return;
 
-    this.classList.add('flip');
+    this.classList.add('flipped');
 
     if (!firstCard) {
         firstCard = this;
